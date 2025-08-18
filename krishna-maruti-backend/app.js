41f7507079b5ff7d5176cdaf88dce0025eac2865
Shubham Bera <121177037+Shubham-2004@users.noreply.github.com>
2025-08-18 13:18:44 +0530

    Update : app.js

--- krishna-maruti-backend/app.js
+++ krishna-maruti-backend/app.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:4200', 'http://localhost:3000'],
+  origin: ['http://localhost:4200', 'http://localhost:3000','https://krishna-maruti-backend.onrender.com'],
   credentials: true
 }));
 app.use(express.json());
